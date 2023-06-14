2d3a23edc0021d4860010580ee340d45bbb782af
Roland Amini <aminiroland01@gmail.com>
2023-06-14 06:35:49 -0700

    done

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
-const accecckey ="ED6L6eSmariHasI7YWoj-1q_2j7w5clFge3lQFYVoVY"
+const accessKey = "ED6L6eSmariHasI7YWoj-1q_2j7w5clFge3lQFYVoVY";
 
-constformEl= document.querySelector("form");
+const formEl = document.querySelector("form");
 const inputEl = document.getElementById("Search-input");
-const SearchResults = document.querySelector(".search-results");
-const showMoere = document.getElementById("show-more-button");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
 
 let inputData = "";
 let page = 1;
 
-async function searchImages(){
- inputData = inputEl.ariaValue;
- const url = 'https//api.unsplash.com/search/photos?page=${page}&query=${inputData}$client id=${accesskey}';
+async function searchImages() {
+  inputData = inputEl.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-const response = await fetch(url);
-const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-const results = data.results;
+  const results = data.results;
 
-if (page===1){
-    searchResolts.innerHTML ="";
-}
+  if (page === 1) {
+    searchResults.innerHTML = "";
+  }
 
-results.map((results)=>{
+  results.map((result) => {
     const imageWrapper = document.createElement('div');
-    imageWrapper.classList.add ('search-result');
+    imageWrapper.classList.add('search-result');
     const image = document.createElement('img');
     image.src = result.urls.small;
     image.alt = result.alt_description;
     const imageLink = document.createElement('a');
-    imageLink.href = result.links.html; 
+    imageLink.href = result.links.html;
     imageLink.target = "_blank";
     imageLink.textContent = result.alt_description;
 
-
     imageWrapper.appendChild(image);
     imageWrapper.appendChild(imageLink);
-    imageWrapper.appendChild(imageWrapper);
+    searchResults.appendChild(imageWrapper);
+  });
+
+  page++;
+  if (page > 1) {
+    showMore.style.display = "block";
+  }
+}
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  page = 1;
+  searchImages();
 });
 
-page++
-if(page > 1){
-    showMoere.style.display = "block"
-}
-}
-
-formEL.addEventListener("submit", (event) =>{
-    event.preventDefault()
-    page -1;
-    searchImages()
-})
-
-showMore.addEventListener("click", () =>{
-   searchImages()
-})
+showMore.addEventListener("click", () => {
+  searchImages();
+});
